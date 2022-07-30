897f51fba860af314ea139d5a78efe398e1d5b6d
Jordan Gonzalez <jordan.olaves91@gmail.com>
2022-07-29 21:36:33 -0500

    gettersAndSettersInGO

--- course/course.go
+++ course/course.go
@@ -11,32 +11,58 @@ import "fmt"
 // un identificador exportado es donde
 // su primer caracter sea en mayúscula
 type course struct {
-	Name    string
-	Price   float64
-	IsFree  bool
-	UserIDs []uint
-	Classes map[uint]string
+	name    string
+	price   float64
+	isFree  bool
+	userIDs []uint
+	classes map[uint]string
 }
 
+// Estos son setters en GO
+func (c *course) SetName(name string) {
+	c.name = name
+}
+func (c *course) SetClasses(classes map[uint]string) {
+	c.classes = classes
+}
+func (c *course) SetUserIDs(userIDs []uint) {
+	c.userIDs = userIDs
+}
+func (c *course) SetPrice(price float64) {
+	c.price = price
+}
+
+// Este es un gettter con GO
+func (c *course) Name() string {
+	return c.name
+}
+func (c *course) Price() float64 {
+	return c.price
+}
+func (c *course) IsFree() bool {
+	return c.isFree
+}
+func (c *course) UserIDs() []uint {
+	return c.userIDs
+}
+
+// Esto es un constructor para cursos
+// con otras validaciones en GO
 func New(name string, price float64, isFree bool) *course {
 	if price == 0 {
 		price = 30
 	}
 	return &course{
-		Name:   name,
-		Price:  price,
-		IsFree: isFree,
+		name:   name,
+		price:  price,
+		isFree: isFree,
 	}
 }
 
 func (c *course) PrintClasses() {
 	text := "Las clases son: "
-	for _, class := range c.Classes {
+	for _, class := range c.classes {
 		text += class + ", "
 	}
 	fmt.Println(text[:len(text)-2])
-}
-
-func (c *course) ChangePrice(price float64) {
-	c.Price = price
 }
--- main.go
+++ main.go
@@ -9,13 +9,16 @@ import (
 func main() {
 	Go := course.New("Go desde cero", 12.34, false)
 
-	Go.UserIDs = []uint{12, 56, 898}
-	Go.Classes = map[uint]string{
+	Go.SetUserIDs([]uint{12, 56, 898})
+	Go.SetClasses(map[uint]string{
 		1: "Introducción",
 		2: "Estructuras",
 		3: "Maps",
-	}
+	})
+	Go.SetPrice(32.14)
 
+	// Esto era cuando no teníamos un constructor
+	// para las estructuras
 	// Go := &course.Course{
 	// 	Name:    "Go desde cero",
 	// 	Price:   12.34,
@@ -27,7 +30,8 @@ func main() {
 	// 		3: "Maps",
 	// 	},
 	// }
-
-	//Go.PrintClasses()
+	Go.SetName("POO con GO")
+	fmt.Println(Go.Name())
+	Go.PrintClasses()
 	fmt.Printf("%+v", Go)
 }
